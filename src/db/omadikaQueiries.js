c49600f77c8becd5d78c909c7335a55c442d0f27
Nvourlidas <nvourlidas@gmail.com>
2024-01-23 15:05:29 +0200

    234

--- src/db/omadikaQueiries.js
+++ src/db/omadikaQueiries.js
@@ -1,14 +1,16 @@
 const db = require('./db');
 
-function getAllOmadika() {
-    const sql = `SELECT *
+function getAllOmadika(id) {
+    const sql = `SELECT *, DATE_FORMAT(startdate, '%d-%m-%Y') AS startdate, DATE_FORMAT(enddate, '%d-%m-%Y') AS enddate, DATE_FORMAT(paydate, '%d-%m-%Y') AS paydate, DATE_FORMAT(birthday, '%d-%m-%Y') AS birthday
     FROM customer
     JOIN omadika ON customer.cid = omadika.cuid
-    JOIN contracts ON contracts.conid = omadika.coid;
+    JOIN contracts ON contracts.conid = omadika.coid
+    WHERE coid = ?
+    ;
     `;
 
     return new Promise((resolve, reject) => {
-        db.query(sql, (err, results) => {
+        db.query(sql,[id], (err, results) => {
             if (err) {
                 reject(err);
             } else {
@@ -18,6 +20,39 @@ function getAllOmadika() {
     });
 }
 
+function DeleteOmadika(id) {
+    const query = `DELETE FROM omadika WHERE cuid = ?`;
+
+    return new Promise((resolve, reject) => {
+        db.query(query, [id], (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
+
+function AddOmadiko(omadikaData) {
+    const  coid  = omadikaData.coid;
+    const cuid = omadikaData.cuid;
+    const sql = `INSERT INTO omadika (coid, cuid) VALUES (?,?)`;
+
+    return new Promise((resolve, reject) => {
+        db.query(sql, [coid, cuid], (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results.insertId); 
+            }
+        });
+    });
+}
+
 module.exports = {  
-    getAllOmadika
+    getAllOmadika,
+    DeleteOmadika,
+    AddOmadiko
 };
--- src/routers/omadika.js
+++ src/routers/omadika.js
@@ -1,15 +1,43 @@
 const express = require('express')
 const router = new express.Router()
-const {getAllOmadika} = require('../db/omadikaQueiries')
+const {getAllOmadika, DeleteOmadika, AddOmadiko} = require('../db/omadikaQueiries')
 
 //GET OMADIKA
-router.get('/omadika', async(req,res) =>{
+router.get('/omadika/:id', async(req,res) =>{
+    const id = req.params.id
     try{
-        const contracts = await getAllOmadika();
-        res.status(200).json(contracts);
+        const omadika = await getAllOmadika(id);
+        res.status(200).json(omadika);
     }catch(e){
         res.status(500).send(e)
     }
 })
+
+
+router.delete('/omadika',async (req,res) => {
+    const id = req.body.id
+    try{
+      const omadikadel = await DeleteOmadika(id)
+      res.status(200).send(omadikadel)
+  
+    }catch(e){
+      console.log(e)
+      res.status(500).send(e)
+    }
+  })
+
+
+  router.post('/omadika', async (req, res) => {
+    const omadikaData = req.body;
+
+    try {
+        const newBranchId = await AddOmadiko(omadikaData);
+        res.status(201).send({ id: newBranchId }); // 201 status for successful creation
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({ error: e });
+    }
+});
+
 
 module.exports=router
