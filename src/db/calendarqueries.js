84c4409d0437f8f5ebd2922f6510c3a159ba51f2
Nvourlidas <nvourlidas@gmail.com>
2024-01-24 18:15:24 +0200

    888

--- src/db/calendarqueries.js
+++ src/db/calendarqueries.js
@@ -2,7 +2,7 @@ const db = require('./db');
 
 function insertEvent(data, callback) {
     const insertQuery = `
-      INSERT INTO Users (title, start, end)
+      INSERT INTO calendar (title, start, end)
       VALUES (?, ?, ?)
     `;
     db.query(insertQuery, [...Object.values(data)], callback);
